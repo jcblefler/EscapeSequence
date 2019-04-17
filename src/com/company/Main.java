81301445335d43652b7d106c5fcf36e4bc876952
GBTC440014UR <GBTC440014UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-04-17 11:01:07 -0400

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        System.out.print("*********\n*\t\t*\n*\t\t*\n*\t\t*\n*\t\t*\n*********\n\n");
+
+        System.out.print("  ***\n *   *\n*\t  *\n*\t  *\n *   *\n  ***\n\n");
+
+        System.out.print("   *\n  * *\n *   *\n  * *\n   *");
+
+    }
+}
